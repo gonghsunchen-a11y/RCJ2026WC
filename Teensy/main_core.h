bdafcafb4c48965791d15336edf190d2d0e22b5f
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2026-06-17 14:16:03 +0800

    Update main_core.h

--- Teensy/main_core.h
+++ Teensy/main_core.h
@@ -1,5 +1,5 @@
 
 struct BallData{uint16_t angle = 255; uint16_t possession = 255; bool valid = false; float Vx; float Vy;} ballData;
 
-#define COM_Pin1
-#define COM_Pin2
+#define COM_Pin1 37
+#define COM_Pin2 36
