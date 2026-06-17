971340c1f45d55aff77b85bfe8271e4d65003c25
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2026-06-17 14:02:44 +0800

    Update main_core.h

--- Teensy/main_core.h
+++ Teensy/main_core.h
@@ -1,19 +1,1 @@
 
-//Motor1
-#define DIR_1 37   // 方向控制腳1
-#define pwmPin1 4    // PWM 控制腳
-
-//Motor2
-#define DIR_2 11    // 方向控制腳2
-#define pwmPin2 6    // PWM 控制腳
-
-//Motor3
-#define DIR_3 10    // 方向控制腳3
-#define pwmPin3 5    // PWM 控制腳
-
-//Motor4
-#define DIR_4 36    // 方向控制腳4
-#define pwmPin4 3    // PWM 控制腳
-
-#define SLP1 23    
-#define SLP2 12
